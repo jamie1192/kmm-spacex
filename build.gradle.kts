7577b9624d2aa71ecd2dc9cfc246a58658a32707
jamie1192 <jastley92@gmail.com>
2021-11-01 15:25:11 +1100

    Upgrade gradle version
    - commit misc updated files

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
-        classpath("com.android.tools.build:gradle:7.0.0-alpha15")
+        classpath("com.android.tools.build:gradle:7.0.3")
 
         classpath("com.squareup.sqldelight:gradle-plugin:$sqlDelightVersion")
 
